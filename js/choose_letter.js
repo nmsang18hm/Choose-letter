bb9c8bbb15673568e1e7440f4d336935fb9d563d
nmsang18hm <57668387+nmsang18hm@users.noreply.github.com>
2020-06-30 13:19:11 +0700

    #bug thêm nút play để fix lỗi âm thanh trên chrome

--- js/choose_letter.js
+++ js/choose_letter.js
@@ -66,6 +66,8 @@ var objletter = document.getElementsByClassName("letter");
 var objbgvictory = document.getElementById("victory");
 var objrestart = document.getElementById("restart");
 var objkodi = document.getElementById("kodi");
+var objplay = document.getElementById("playbutton");
+var objbgplay = document.getElementById("bghome");
 
 //Biến nội dung trò chơi
 var key = ['b', 'f'];
@@ -73,98 +75,101 @@ var letter = ['a', 'b', 'c', 'd', 'c', 'f'];
 var stage = 1;
 var limitstage = 2;
 
-// Gán lại các giá trị animation(chưa biết cách khắc phục)
-initial(stage);
+objplay.onclick = function() {
+	objbgplay.style.left = "-100vw";
+	// Gán lại các giá trị animation(chưa biết cách khắc phục)
+	initial(stage);
 
-// Xử lý các sự kiện lựa chọn của người chơi
-for(var i = 0; i < objanimal.length; i++) (function(i){
-	objanimal[i].onclick = function() {
-		audioletterV[i].play();
-		if(letter[(stage-1)*3 + i] == key[stage-1]){
-			//Dưới đây là hiệu ứng khi chọn đúng
-			setTimeout(function timer() {
-				audiocorrect.play();
-			}, 1000);
-			setTimeout(function timer() {
-				audiogoodjoob.play();
-			}, 2000);
-			if(stage == 1) {
-				objanimal[i].style.backgroundImage = "url('../res/image/animal/usgk$library$canvas_editor$stickers$images$sticker023_goat-ipad@2x$white.png')";
-			}
-			else {
-				objanimal[i].style.backgroundImage = "url('../res/image/animal/usgk$library$canvas_editor$stickers$images$sticker024_hog-ipad@2x$white.png')";
-			}
-			for(let k=1; k<700; k++) {
-    			setTimeout(function timer(){
-				objanimal[i].style.width = parseFloat(objanimal[i].style.width) + 0.01 + "vw";
-				objanimal[i].style.left = parseFloat(objanimal[i].style.left) - 0.005 + "vw";
-				objletter[i].style.width = parseFloat(objletter[i].style.width) + 0.00265 + "vw";
-				objletter[i].style.left = parseFloat(objletter[i].style.left) + 0.003675 + "vw";
-				objanimal[i].style.height = parseFloat(objanimal[i].style.height) + 0.01 + "vh";
-				objanimal[i].style.top = parseFloat(objanimal[i].style.top) - 0.005 + "vh";
-				objletter[i].style.height = parseFloat(objletter[i].style.height) + 0.00422 + "vh";
-				objletter[i].style.top = parseFloat(objletter[i].style.top) + 0.00289 + "vh";
-    			}, k*0.5 );
-			}
-			//Dưới đây là phần chuyển màn chơi
-			stage++;
-			setTimeout(function timer(){
-				if(stage <= limitstage) {
-					initial(stage);
-					objbackground.style.backgroundImage = "url('../res/image/background/usgk$library$canvas_editor$backgrounds$images$22purplebg@2x.png')";
-					for(var j = 0; j < 3; j++) {
-						objanimal[j].style.backgroundImage = "url('../res/image/animal/usgk$library$canvas_editor$stickers$images$sticker024_hog-ipad@2x.png')";
-					}
+	// Xử lý các sự kiện lựa chọn của người chơi
+	for(var i = 0; i < objanimal.length; i++) (function(i){
+		objanimal[i].onclick = function() {
+			audioletterV[i].play();
+			if(letter[(stage-1)*3 + i] == key[stage-1]){
+				//Dưới đây là hiệu ứng khi chọn đúng
+				setTimeout(function timer() {
+					audiocorrect.play();
+				}, 1000);
+				setTimeout(function timer() {
+					audiogoodjoob.play();
+				}, 2000);
+				if(stage == 1) {
+					objanimal[i].style.backgroundImage = "url('../res/image/animal/usgk$library$canvas_editor$stickers$images$sticker023_goat-ipad@2x$white.png')";
 				}
 				else {
-					audiocongratulation.play();
-					objbgvictory.style.left = "8.5vw";
-					objbgvictory.style.top = "15vh";
+					objanimal[i].style.backgroundImage = "url('../res/image/animal/usgk$library$canvas_editor$stickers$images$sticker024_hog-ipad@2x$white.png')";
 				}
-			}, 4000)
-		}
-		else {
-			//Đoạn code dưới đây là animation phóng to rồi thu nhỏ con vật khi chọn sai
-			setTimeout(function timer() {
-				audioincorrect.play();
-			}, 1000);
-			setTimeout(function timer() {
-				audiotryagain.play();
-			}, 2000);
-			for(let k=1; k<400; k++) {
-    			setTimeout(function timer(){
-				objanimal[i].style.width = parseFloat(objanimal[i].style.width) + 0.01 + "vw";
-				objanimal[i].style.left = parseFloat(objanimal[i].style.left) - 0.005 + "vw";
-				objletter[i].style.width = parseFloat(objletter[i].style.width) + 0.00265 + "vw";
-				objletter[i].style.left = parseFloat(objletter[i].style.left) + 0.003675 + "vw";
-				objanimal[i].style.height = parseFloat(objanimal[i].style.height) + 0.01 + "vh";
-				objanimal[i].style.top = parseFloat(objanimal[i].style.top) - 0.005 + "vh";
-				objletter[i].style.height = parseFloat(objletter[i].style.height) + 0.00422 + "vh";
-				objletter[i].style.top = parseFloat(objletter[i].style.top) + 0.00289 + "vh";
-    			}, k*0.5 );
+				for(let k=1; k<700; k++) {
+					setTimeout(function timer(){
+					objanimal[i].style.width = parseFloat(objanimal[i].style.width) + 0.01 + "vw";
+					objanimal[i].style.left = parseFloat(objanimal[i].style.left) - 0.005 + "vw";
+					objletter[i].style.width = parseFloat(objletter[i].style.width) + 0.00265 + "vw";
+					objletter[i].style.left = parseFloat(objletter[i].style.left) + 0.003675 + "vw";
+					objanimal[i].style.height = parseFloat(objanimal[i].style.height) + 0.01 + "vh";
+					objanimal[i].style.top = parseFloat(objanimal[i].style.top) - 0.005 + "vh";
+					objletter[i].style.height = parseFloat(objletter[i].style.height) + 0.00422 + "vh";
+					objletter[i].style.top = parseFloat(objletter[i].style.top) + 0.00289 + "vh";
+					}, k*0.5 );
+				}
+				//Dưới đây là phần chuyển màn chơi
+				stage++;
+				setTimeout(function timer(){
+					if(stage <= limitstage) {
+						initial(stage);
+						objbackground.style.backgroundImage = "url('../res/image/background/usgk$library$canvas_editor$backgrounds$images$22purplebg@2x.png')";
+						for(var j = 0; j < 3; j++) {
+							objanimal[j].style.backgroundImage = "url('../res/image/animal/usgk$library$canvas_editor$stickers$images$sticker024_hog-ipad@2x.png')";
+						}
+					}
+					else {
+						audiocongratulation.play();
+						objbgvictory.style.left = "8.5vw";
+						objbgvictory.style.top = "15vh";
+					}
+				}, 4000)
 			}
-			setTimeout(function timer() {
+			else {
+				//Đoạn code dưới đây là animation phóng to rồi thu nhỏ con vật khi chọn sai
+				setTimeout(function timer() {
+					audioincorrect.play();
+				}, 1000);
+				setTimeout(function timer() {
+					audiotryagain.play();
+				}, 2000);
 				for(let k=1; k<400; k++) {
-    			setTimeout(function timer(){
-				objanimal[i].style.width = parseFloat(objanimal[i].style.width) - 0.01 + "vw";
-				objanimal[i].style.left = parseFloat(objanimal[i].style.left) + 0.005 + "vw";
-				objletter[i].style.width = parseFloat(objletter[i].style.width) - 0.00265 + "vw";
-				objletter[i].style.left = parseFloat(objletter[i].style.left) - 0.003675 + "vw";
-				objanimal[i].style.height = parseFloat(objanimal[i].style.height) - 0.01 + "vh";
-				objanimal[i].style.top = parseFloat(objanimal[i].style.top) + 0.005 + "vh";
-				objletter[i].style.height = parseFloat(objletter[i].style.height) - 0.00422 + "vh";
-				objletter[i].style.top = parseFloat(objletter[i].style.top) - 0.00289 + "vh";
-    			}, k*0.5 );
+					setTimeout(function timer(){
+					objanimal[i].style.width = parseFloat(objanimal[i].style.width) + 0.01 + "vw";
+					objanimal[i].style.left = parseFloat(objanimal[i].style.left) - 0.005 + "vw";
+					objletter[i].style.width = parseFloat(objletter[i].style.width) + 0.00265 + "vw";
+					objletter[i].style.left = parseFloat(objletter[i].style.left) + 0.003675 + "vw";
+					objanimal[i].style.height = parseFloat(objanimal[i].style.height) + 0.01 + "vh";
+					objanimal[i].style.top = parseFloat(objanimal[i].style.top) - 0.005 + "vh";
+					objletter[i].style.height = parseFloat(objletter[i].style.height) + 0.00422 + "vh";
+					objletter[i].style.top = parseFloat(objletter[i].style.top) + 0.00289 + "vh";
+					}, k*0.5 );
+				}
+				setTimeout(function timer() {
+					for(let k=1; k<400; k++) {
+					setTimeout(function timer(){
+					objanimal[i].style.width = parseFloat(objanimal[i].style.width) - 0.01 + "vw";
+					objanimal[i].style.left = parseFloat(objanimal[i].style.left) + 0.005 + "vw";
+					objletter[i].style.width = parseFloat(objletter[i].style.width) - 0.00265 + "vw";
+					objletter[i].style.left = parseFloat(objletter[i].style.left) - 0.003675 + "vw";
+					objanimal[i].style.height = parseFloat(objanimal[i].style.height) - 0.01 + "vh";
+					objanimal[i].style.top = parseFloat(objanimal[i].style.top) + 0.005 + "vh";
+					objletter[i].style.height = parseFloat(objletter[i].style.height) - 0.00422 + "vh";
+					objletter[i].style.top = parseFloat(objletter[i].style.top) - 0.00289 + "vh";
+					}, k*0.5 );
+				}
+				},175);
 			}
-			},175);
 		}
+	})(i);
+
+	objrestart.onclick = function() {
+		window.location = document.URL;
 	}
-})(i);
 
-objrestart.onclick = function() {
-	window.location = document.URL;
-}
-
-objkodi.onclick = function() {
-	talkwhichone();
+	objkodi.onclick = function() {
+		talkwhichone();
+	}
 }
